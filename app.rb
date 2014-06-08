34afa0a375b8ca1115bc3e3a24a167b2668c5621
Arnaud Benard <arnaudbenard13@gmail.com>
2014-06-08 22:53:30 +0100

    cleaning that tube api

--- app.rb
+++ app.rb
@@ -2,7 +2,16 @@ require 'sinatra'
 require 'httparty'
 require 'crack'
 
-get '/' do
-    response = HTTParty.get('http://cloud.tfl.gov.uk/TrackerNet/LineStatus')
-    Crack::XML.parse(response.body).to_json
+get '/status' do
+    content_type :json
+
+    response = []
+
+    responseTFL = HTTParty.get('http://cloud.tfl.gov.uk/TrackerNet/LineStatus')
+
+    Crack::XML.parse(responseTFL.body)["ArrayOfLineStatus"]["LineStatus"].each do |branch|
+        response << branch['Line']['Name']
+    end
+
+    {"result" => response}.to_json
 end
